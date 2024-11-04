237054bad5e2b082fbc5bf73d652e1937bbff5b1
felixmathew03 <felixmathewtt@gmail.com>
2024-11-04 14:43:53 +0530

    4

--- flipkart/App.jsx
+++ flipkart/App.jsx
@@ -6,18 +6,19 @@ class App extends Component{
     super(props);
     this.state={
       inputVal:"",
-      items:[]
+      items:[],
+      category:[]
     }
   }
   async componentDidMount(){
     const res=await fetch('https://dummyjson.com/products');
     const data=await res.json();
-    this.setState({items:[...data.products]})
+    this.setState({items:[...data.products],category:[...new Set(data.products.map(item => item.category))]})
   }
   render(){
     return <><nav>
       <h3>Flip.com</h3>
-      <input class="input" name="text" placeholder="Search for products and more..." type="search"/>
+      <input className="input" name="text" placeholder="Search for products and more..." type="search"/>
       <div className="log">
         <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/profile-52e0dc.svg" alt="" /><p>Login</p>
       </div>
@@ -28,6 +29,26 @@ class App extends Component{
         <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/header_3verticalDots-ea7819.svg" alt="" />
       </div>
     </nav>
+      {this.state.category.map((cat,ind)=>{
+        const arr=[...this.state.items.filter((item) => item.category == cat)];
+        console.log(arr);
+        
+        return <div className="categories" key={ind}>
+          <h2>{cat.toUpperCase()}</h2>
+          <hr />
+          <div className="category">
+          {arr.map((item,ind)=>{
+            return <div className="item" key={ind}>
+            <img src={item.thumbnail} alt="" />
+            <p>{item.title.substring(0,20)}</p>
+            <p><b>${item.price}</b></p>
+            </div>
+        })}
+        </div>
+        </div>
+      })}
+        <h2 align="center" style={{marginBottom:"1%"}}><u>ALL PRODUCTS</u> </h2>
+        
       <div className="main">
         {this.state.items.map((item,ind)=>{
           return <div className="item" key={ind}>
