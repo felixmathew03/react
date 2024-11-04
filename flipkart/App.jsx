bdd248dc26792ddcd2b04b3949df138866a7b009
felixmathew03 <felixmathewtt@gmail.com>
2024-11-04 09:03:48 +0530

    3

--- flipkart/App.jsx
+++ flipkart/App.jsx
@@ -15,15 +15,29 @@ class App extends Component{
     this.setState({items:[...data.products]})
   }
   render(){
-    console.log(this.state.items);
-    return <div className="main">
-      {this.state.items.map((item)=>{
-        return <div className="item">
-          <h1>{item.title.substring(0,15)}</h1>
-          <img src={item.thumbnail} alt="" />
-          </div>
-      })}
-    </div>
+    return <><nav>
+      <h3>Flip.com</h3>
+      <input class="input" name="text" placeholder="Search for products and more..." type="search"/>
+      <div className="log">
+        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/profile-52e0dc.svg" alt="" /><p>Login</p>
+      </div>
+      <div className="log">
+        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/header_cart-eed150.svg" alt="" /><p>Cart</p>
+      </div>
+      <div className="log">
+        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/header_3verticalDots-ea7819.svg" alt="" />
+      </div>
+    </nav>
+      <div className="main">
+        {this.state.items.map((item,ind)=>{
+          return <div className="item" key={ind}>
+            <img src={item.thumbnail} alt="" />
+            <p>{item.title.substring(0,20)}</p>
+            <p><b>${item.price}</b></p>
+            </div>
+        })}
+      </div>
+    </>
   }
 }
 
