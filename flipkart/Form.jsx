88716b4bddcbd949d695056f8517e8cfadd5e184
felixmathew03 <felixmathewtt@gmail.com>
2024-11-05 11:16:58 +0530

    Update Form.jsx

--- flipkart/Form.jsx
+++ flipkart/Form.jsx
@@ -1,7 +1,7 @@
 import React,{Component} from "react";
 
 class Form extends Component {
-    state={name:"",age:""}
+    state={name:"",age:"",email:"",phone:""}
     handleSubmit=(e)=>{
         e.preventDefault();
         console.log(this.state.name);
@@ -16,6 +16,8 @@ class Form extends Component {
                 <form >
                     <input type="text" name="name"  placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
                     <input type="text" name="age"  placeholder="Age" value={this.state.age} onChange={this.handleChange}/>
+                    <input type="email" name="email"  placeholder="Email" value={this.state.age} onChange={this.handleChange}/>
+                    <input type="number" name="phone"  placeholder="Phone" value={this.state.age} onChange={this.handleChange}/>
                     <button onClick={this.handleSubmit}>Submit</button>
                 </form>
             </div>
