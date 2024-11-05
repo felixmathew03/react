00f153a24da59aa28125798b075479f247ab8dbf
felixmathew03 <felixmathewtt@gmail.com>
2024-11-05 11:02:27 +0530

    Update App1.jsx

--- flipkart/App1.jsx
+++ flipkart/App1.jsx
@@ -15,10 +15,24 @@ class App extends Component{
     const data=await res.json();
     this.setState({items:[...data.products],category:[...new Set(data.products.map(item => item.category))]})
   }
+  handleChange=(evnt)=>{
+    this.setState({inputVal:evnt.target.value});
+    let str=``;
+    this.state.items.filter((i)=>i.title.toLowerCase().includes(evnt.target.value.toLowerCase())).map((item)=>{
+      str+=`
+        <div class="item" >
+              <img src=${item.thumbnail} alt="" />
+              <p>${item.title.substring(0,20)}</p>
+              <p><b>$${item.price}</b></p>
+              </div>`
+      
+    })
+    document.getElementById("main").innerHTML=str;
+  }
   render(){
     return <><nav>
       <h3>Flip.com</h3>
-      <input className="input" name="text" placeholder="Search for products and more..." type="search"/>
+      <input className="input" name="text" placeholder="Search for products and more..."  value={this.state.inputVal} onChange={this.handleChange}type="search"/>
       <div className="log">
         <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/profile-52e0dc.svg" alt="" /><p>Login</p>
       </div>
@@ -29,9 +43,9 @@ class App extends Component{
         <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/header_3verticalDots-ea7819.svg" alt="" />
       </div>
     </nav>
-    <h2 align="center" style={{marginBottom:"1%"}}><u>ALL PRODUCTS</u> </h2>
         
-        <div className="main">
+    <h2 align="center" style={{marginBottom:"1%"}}><u>ALL PRODUCTS</u> </h2>
+        <div className="main" id='main'>
           {this.state.items.map((item,ind)=>{
             return <div className="item" key={ind}>
               <img src={item.thumbnail} alt="" />
@@ -42,8 +56,6 @@ class App extends Component{
         </div>
       {this.state.category.map((cat,ind)=>{
         const arr=[...this.state.items.filter((item) => item.category == cat)];
-        console.log(arr);
-        
         return <div className="categories" key={ind}>
           <h2>{cat.toUpperCase()}</h2>
           <hr />
