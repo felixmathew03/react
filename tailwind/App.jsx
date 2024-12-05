d574b5fa196e6b18e91fc115cbb7686ecf00c8b2
felixmathew03 <felixmathewtt@gmail.com>
2024-12-05 09:11:38 +0530

    g

---
+++ tailwind/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-32 h-32 m-7 px-5 py-12 flex space-x-4 border border-2 border-purple-600 rounded-full">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+    </div>
+  )
+}
+
+export default App
